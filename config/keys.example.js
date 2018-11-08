4edfb28da8f39d8660438458e87292f7a4b8d164
michaelluong <mluong517@gmail.com>
2018-11-07 23:49:25 -0800

    Added instructions in keys.example.js

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,4 +1,7 @@
-// Insert your MongoDB URI below.
+/** SETUP:
+ *      1) Duplicate this file and save as: "keys.js"
+ *      2) Insert your MongoDB URI into mongoURI.
+ */
 module.exports = {
   mongoURI: 'mongodb://<dbuser>:<dbpassword>@...'
 };
